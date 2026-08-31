e3b52cf9843a62df1d8eb1d7d11ba167696def73
kuteaditya5-png <kute.aditya5@gmail.com>
2026-09-01 01:53:37 +0530

    Delete scheduled_alert_whatsapp_patch.js

--- scheduled_alert_whatsapp_patch.js
+++
@@ -1,9 +0,0 @@
-// In your existing checkScheduledAlert() function,
-// immediately AFTER:
-//     await loadFnoAlerts(true);
-//
-// add:
-await fetch(
-    "/api/whatsapp/evaluate",
-    { method: "POST" }
-);
